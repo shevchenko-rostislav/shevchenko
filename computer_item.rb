61287d74d0084e8c6da0af39f61cf0134e0f22be
ShevchenkoR <shevchenkorostislav1991@gmail.com>
2015-07-01 13:25:55 +0300

    first commit

---
+++ computer_item.rb
@@ -0,0 +1,27 @@
+class ComputerItems
+
+  attr_accessor :items,:name,:price
+
+  def initialize(options={})
+    @items=[]
+    @name=options[:name]
+    @price=options[:price]
+  end
+
+  def add_items(item)
+    @items.push item
+  end
+
+  def delete_items
+    @items.pop
+  end
+
+end
+
+@items=ComputerItems.new
+
+@items.add_items(ComputerItems.new)
+@items.add_items(ComputerItems.new)
+p @items
+@items.delete_items
+p @items
